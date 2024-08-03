3da7873e99d8dee40dd3fdbcf2e9b4615c5e4c35
Manu <mandrescabral@gmail.com>
2024-08-03 16:29:23 -0300

    feat: update main entry point

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from sys import exit
+import logging
 import rpp
 
-if __name__ == "__main__":
 
-    log = rpp.getLogger("Main")
+def checkBrowser(log: logging.Logger, browserId: str) -> None:
+    """
+    Simple check to see if a browser is found
+
+    Args:
+        log (logging.Logger): Logger instance
+        browserId (str): Browser ID
+
+    Returns:
+        None
+    """
     log.info("Checking for browser...")
-    browserId = rpp.getBrowserProgId()
     if not browserId:
         log.info("Browser not found")
         exit()
 
-    log.info(f"Browser found: {browserId}")
-    print("Browser tools")
+
+def browserTools(log: logging.Logger, browserId: str) -> None:
+    """
+    Browser tools to help with debugging
+
+    Args:
+        log (logging.Logger): Logger instance
+        browserId (str): Browser ID
+
+    Returns:
+        None
+    """
+    print(f"Browser tools for {browserId}")
     print("1. Close and open browser")
     print("2. Close all browser instances")
     print("3. Open browser with remote debugging")
@@ -24,14 +44,25 @@ if __name__ == "__main__":
     elif res == "2":
         log.info("Closing all browser instances")
         rpp.killBrowser(rpp.getBrowserProcess(browserId))
+        rpp.info("Please restart the application")
+        input("Press any key to exit...")
+        exit()
     elif res == "3":
         log.info("Opening browser with remote debugging")
         rpp.openRemoteDebuggingPort(9222)
     else:
-        log.info("Skipping browser setup")
+        log.info("Skipping browser tools")
+
+
+if __name__ == "__main__":
+
+    log = rpp.getLogger("Main")
+    browserId = rpp.getBrowserProgId()
+    checkBrowser(log, browserId)
+    browserTools(log, browserId)
 
     runtime = rpp.Runtime(9222)
-    main = rpp.Manager(runtime=runtime, dev=True)
+    main = rpp.Manager(runtime=runtime, dev=False)
     main.load()
 
     if len(main.presences) == 0:
