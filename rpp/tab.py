a7cae52af918c86894e9c994da639d371d94cef2
Manu <mandrescabral@gmail.com>
2024-08-07 18:27:21 -0300

    refactor: update tab

--- rpp/tab.py
+++ rpp/tab.py
@@ -1,35 +1,111 @@
+class Attribute:
+    """
+    Attribute object.
+
+    Args:
+        name (str): The name of the attribute.
+        type (str): The type of the attribute.
+        value (str): The value of the attribute.
+    """
+
+    __slots__ = ("name", "type", "value")
+
+
+class PropertiesResponse:
+    """
+    Dynamic properties response object.
+
+    All attributes are a instance of Attribute class.
+    """
+
+
+class RemoteObject:
+    """
+    Remote object class.
+    """
+
+    __slots__ = (
+        "type",
+        "subtype",
+        "className",
+        "value",
+        "unserializableValue",
+        "description",
+        "objectId",
+    )
+
+
 class Tab:
     """
     A class representing a tab in the browser.
     """
 
-    def __init__(self, **kwargs: dict) -> None:
-        self.__id = kwargs.get("id", None)
-        self.__url = kwargs.get("url", "about:blank")
-        self.__title = kwargs.get("title", "Unknown")
-
     @property
     def id(self) -> str:
         """
-        Get the id of the tab.
+        Get the tab id.
+
+        Returns:
+            str: The tab id.
         """
-        return self.__id
+        pass
 
     @property
     def url(self) -> str:
         """
-        Get the url of the tab.
+        Get the tab url.
+
+        Returns:
+            str: The tab url.
         """
-        return self.__url
+        pass
 
     @property
     def title(self) -> str:
         """
-        Get the title of the tab.
-        """
-        return self.__title
+        Get the tab title.
 
-    def execute(self, code: str) -> str:
+        Returns:
+            str: The tab title.
+        """
+        pass
+
+    @property
+    def connected(self) -> bool:
+        """
+        Check if the tab is connected.
+        If the tab is connected, it means you can execute code in it.
+
+        Returns:
+            bool: True if connected, False otherwise.
+        """
+        pass
+
+    def getProperties(self, objectId: str) -> PropertiesResponse:
+        """
+        Get the properties of an RemoteObject.
+
+        Args:
+            objectId (str): The RemoteObject id.
+
+        Returns:
+            PropertiesResponse: The properties of the object.
+        """
+        pass
+
+    def connect(self) -> None:
+        """
+        Connect to the tab to execute code in it.
+        """
+        pass
+
+    def close(self) -> None:
+        """
+        Close the tab connection.
+        """
+        pass
+
+    def execute(self, code: str) -> RemoteObject:
         """
         Execute javascript code in the tab.
 
@@ -37,6 +113,6 @@ class Tab:
             code (str): The code to execute.
 
         Returns:
-            str: The result of the code execution.
+            RemoteObject: The result of the execution.
         """
         pass
