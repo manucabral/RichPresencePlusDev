c2c139ce8941e42e60c55f7366a694b051273164
Manu <mandrescabral@gmail.com>
2024-10-16 16:55:32 -0300

    refactor: update setup}

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-import rpp
+from main import __title__, __version__, __description__
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
     "excludes": [
         "tkinter",
         "unittest",
+        "pypresence",
         "rpp.Manager",
         "rpp.Runtime",
         "rpp.Presence",
         "rpp.Browser",
+        "rpp.load_env",
+        "rpp.get_available_presences",
     ],
-    "packages": ["pypresence", "websocket", "presences"],
+    "packages": ["websocket", "presences"],
 }
 
 setup(
-    name=rpp.__title__,
-    version=rpp.__version__,
-    description="A simple Discord Rich Presence manager for desktop and web apps.",
+    name=__title__,
+    version=__version__,
+    description=__description__,
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base="Console", target_name="RPPDev")],
+    executables=[Executable("main.py", base="Console", target_name="rpp_dev")],
 )
